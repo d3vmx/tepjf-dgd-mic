0cf4f33fd82232c0f945f75baf35c0ffab619e31
Juan Luis Carrillo García <luis@d3v.mx>
2024-05-24 23:04:28 -0600

    folleto v1.5

---
+++ src/js/folleto.js
@@ -0,0 +1,25 @@
+document.addEventListener("gesturestart", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gesturechange", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gestureend", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 1;
+});
+
+window.addEventListener("touchstart", touchHandler, false);
+
+function touchHandler(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}
+
+// Abre Google Chrome, copia y pega chrome://flags/#enable-pinch en la barra de direcciones y luego presiona Enter.
+// Desplázate hacia abajo, localiza la función de zoom de pellizco (pinch zoom feature) y desactívala.
